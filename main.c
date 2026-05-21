34282d11b4f66d6211a9f61afdb5256f68dbce57
acano-kr <acano-kr@student.42.fr>
2026-05-21 13:35:06 +0100

    atualizando stack.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acano-kr <acano-kr@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 12:11:34 by acano-kr          #+#    #+#             */
-/*   Updated: 2026/05/20 08:39:17 by acano-kr         ###   ########.fr       */
+/*   Updated: 2026/05/21 13:33:28 by acano-kr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,60 @@ static void	error(void)
 	exit(1);
 }
 
+static int  ft_count_args(int argc, char **argv)
+{
+	int		total;
+	int		i;
+	int		j;
+	char	**split;
+
+	total = 0;
+	i = 1;
+	while (i < argc)
+	{
+		split = ft_split(argv[i], ' ');
+		j = 0;
+		while (split[j])
+		{
+			total++;
+			j++;
+		}
+		j = 0;
+		while (split[j])
+			free(split[j++]);
+		free(split);
+		i++;
+	}
+	return (total);
+}
+
+static char	**ft_get_args(int argc, char **argv, int *size)
+{
+	int		i;
+	char	**args;
+	char	*str;
+
+	i = 0;
+	while (argv[i] < argc -1)
+	{
+		args = ft_split(argv[i], ' ');
+		size = ++i;
+	}
+	str = malloc(sizeof(char **) * *args[i] + 1);
+	i = 0;
+	while ()
+}
+
 int	main(int argc, char **argv)
 {
 	int		*values;
 	int		size;
+	int		i;
 	char	**args;
 
-	if (argc < 2)
-		return (0);
-	if (argc == 2)
+	i = 0;
+	while (argv[i])
 		args = ft_split(argv[1], ' ');
-	else
-		args = argv + 1;
 	size = 0;
 	while (args[size])
 		size++;
@@ -41,5 +83,3 @@ int	main(int argc, char **argv)
 	free(values);
 	return (0);
 }
-
-// 29. Se vier tudo numa unica string, senao conta str por str
