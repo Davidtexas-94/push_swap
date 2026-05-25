771bd978a369b88daf1f5c8860ac1199fc74a66a
Ana Filipa <anafilipa@MacBook-Air-de-Ana.local>
2026-05-25 20:06:39 +0100

    corrigindo edge case --simple4

--- srcs/flags.c
+++ srcs/flags.c
@@ -6,7 +6,7 @@
 /*   By: acano-kr <acano-kr@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 11:25:04 by acano-kr          #+#    #+#             */
-/*   Updated: 2026/05/25 15:13:45 by acano-kr         ###   ########.fr       */
+/*   Updated: 2026/05/25 20:00:00 by acano-kr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int	get_flag(int argc, char **argv, t_flags *flags)
 	i = 1;
 	while (i < argc && ft_strncmp(argv[i], "--", 2) == 0)
 	{
-		if (ft_strncmp(argv[i] + 2, "bench", 5) == 0)
+		if (ft_strncmp(argv[i] + 2, "bench", 6) == 0)
 			flags->bench = 1;
-		else if (ft_strncmp(argv[i] + 2, "adaptive", 8) == 0)
+		else if (ft_strncmp(argv[i] + 2, "adaptive", 9) == 0)
 			flags->strategy = FLAG_ADAPTIVE;
-		else if (ft_strncmp(argv[i] + 2, "simple", 6) == 0)
+		else if (ft_strncmp(argv[i] + 2, "simple", 7) == 0)
 			flags->strategy = FLAG_SIMPLE;
-		else if (ft_strncmp(argv[i] + 2, "medium", 6) == 0)
+		else if (ft_strncmp(argv[i] + 2, "medium", 7) == 0)
 			flags->strategy = FLAG_MEDIUM;
-		else if (ft_strncmp(argv[i] + 2, "complex", 7) == 0)
+		else if (ft_strncmp(argv[i] + 2, "complex", 8) == 0)
 			flags->strategy = FLAG_COMPLEX;
 		else
 			return (-1);
