248eec1a3effccc0c9a410592c84a3116302dea2
David Miguel Serra De Sa Teixeira <dserra-d@c1r5s6.42lisboa.com>
2026-05-25 09:49:43 +0100

    algo_simple

--- algorithm/algo_simple.c
+++ algorithm/algo_simple.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   algo_simple.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dserra-d <dserra-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: dserra-d <dserra-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 11:15:17 by dserra-d          #+#    #+#             */
-/*   Updated: 2026/05/22 11:54:01 by dserra-d         ###   ########.fr       */
+/*   Updated: 2026/05/25 09:48:11 by dserra-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,31 @@ void	sort_two(t_stack *a)
 
 void	sort_three(t_stack *a)
 {
-	t_node *x;
-	t_node *y;
-	t_node *z;
+	t_node	*x;
+	t_node	*y;
+	t_node	*z;
 
 	if (!a)
-		return;
+		return ;
 	x = a->top;
 	y = a->top->next;
 	z = a->bottom;
-
+	if (y->value < x->value && y->value < z->value && x->value < z->value)
+		sa (a);
+	else if (x->value < y->value && y->value > z->value && x->value < z->value)
+	{
+		rra (a);
+		sa (a);
+	}
+	else if (x->value < y->value && y->value > z->value && x->value > z->value)
+		rra (a);
+	else if (x->value > y->value && y->value < z->value && x->value > z->value)
+		ra (a);
+	else if (x->value > y->value && y->value > z->value && x->value > z->value)
+	{
+		sa (a);
+		rra (a);
+	}
 }
 
 void	sort_simple(t_stack *a, t_stack *b)
@@ -43,6 +58,7 @@ void	sort_simple(t_stack *a, t_stack *b)
 	else if (a->size == 3)
 		sort_three (a);
 	else
+	{
 		while (a->size > 1)
 		{
 			move_biggest_top(a);
@@ -52,4 +68,5 @@ void	sort_simple(t_stack *a, t_stack *b)
 		{
 			pa(a, b);
 		}
+	}
 }
