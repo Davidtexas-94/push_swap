ffadaa9ced95c4ea731f4dfc32b1eabcc9bb1de1
David_texasit <david.texas.it@gmail.com>
2026-05-26 10:23:39 +0100

    test algo

--- algorithm/algo_medium.c
+++ algorithm/algo_medium.c
@@ -6,13 +6,43 @@
 /*   By: dserra-d <dserra-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 09:45:26 by dserra-d          #+#    #+#             */
-/*   Updated: 2026/05/26 09:49:23 by dserra-d         ###   ########.fr       */
+/*   Updated: 2026/05/26 10:23:06 by dserra-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void    sort_medium(t_stack *a, t_stack *b)
+void	sort_medium(t_stack *a, t_stack *b)
 {
+	int	bits;
+	int	max;
+	int	i;
+	int	k;
 
+	if (!a || !b)
+		return;
+	max = a->size - 1;
+	bits = 0;
+	set_index(a);
+	while (max > 0)
+	{
+		bits++;
+		max >>= 1;
+	}
+	i = 0;
+	while (i < bits)
+	{
+		k = a->size;
+		while (k > 0)
+		{
+			if ((a->top->index >> i) & 1)
+				ra (a);
+			else
+				pb (a, b);
+			k--;
+		}
+		while (b->size > 0)
+			pa (a, b);
+		i++;
+	}
 }
